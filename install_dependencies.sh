a5d96645ba2ebad4905f76a093b4350b362d9370
Vincent Pacelli <vpacelli@princeton.edu>
2019-09-13 15:35:56 -0400

    Added cfclient dependencies / install.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,7 +8,7 @@ cd
 add-apt-repository ppa:team-gcc-arm-embedded/ppa
 apt-get update
 
-apt install -y python3-pip gcc-arm-embedded git python-minimal
+apt install -y python3 python3-pip gcc-arm-embedded git python-minimal python3-pyqt5 python3-pyqt5.qtsvg
 
 pip3 install numpy scipy matplotlib sympy jupyterlab
 
@@ -21,6 +21,15 @@ cd
 git clone --recursive https://github.com/irom-lab/crazyflie-firmware
 cd crazyflie-firmware
 make
+
+##################################################
+#########        INSTALL CFCLIENT        #########
+##################################################
+
+cd
+git clone https://github.com/bitcraze/crazyflie-clients-python.git
+cd crazyflie-clients-python
+pip3 install -e .
 
 ##################################################
 #########        CLONE  NOTEBOOKS        #########
